725a3e016e0e813a3046a6706d644bb1f0b4432e
bobbobbob15 <wd40bug@gmail.com>
2021-10-06 21:01:53 -0600

    started thinking about gson

--- src/main/java/com/github/bobbobbob15/Main.java
+++ src/main/java/com/github/bobbobbob15/Main.java
@@ -4,10 +4,25 @@ package com.github.bobbobbob15;
 import javax.mail.Authenticator;
 import javax.mail.PasswordAuthentication;
 import javax.mail.Session;
+import java.io.*;
 import java.util.Properties;
 
 public class Main {
-    public static void main(String[] args) {
+    static String loginFile = "C:/Users/wd40b/eclipse-workspace/Email/src/main/java/com/github/bobbobbob15/login.txt";
+    public static void main(String[] args) throws IOException, ClassNotFoundException {
+        FileOutputStream fileOutputStream = new FileOutputStream(loginFile);
+        ObjectOutputStream objectOutputStream = new ObjectOutputStream(fileOutputStream);
+        var user = new User();
+        user.setUsername("wd50bug@gmail.com");
+        user.setPassword("mR2D8Z5RhLrnjzw");
+        objectOutputStream.writeObject(user);
+        objectOutputStream.flush();
+        objectOutputStream.close();
+        FileInputStream fileInputStream = new FileInputStream(loginFile);
+        ObjectInputStream objectInputStream = new ObjectInputStream(fileInputStream);
+        user = (User) objectInputStream.readObject();
+        objectInputStream.close();
+        System.out.println(user.getUsername()+", "+user.getPassword());
         Properties prop = new Properties();
         prop.put("mail.smtp.auth", true);
         prop.put("mail.smtp.starttls.enable", "true");
@@ -15,10 +30,11 @@ public class Main {
         prop.put("mail.smtp.port", "25");
         prop.put("mail.smtp.ssl.trust", "smtp.gmail.com");
 
+        User finalUser = user;
         Session session = Session.getInstance(prop, new Authenticator() {
             @Override
             protected PasswordAuthentication getPasswordAuthentication() {
-                return new PasswordAuthentication("wd50bug@gmail.com", "mR2D8Z5RhLrnjzw");
+                return new PasswordAuthentication(finalUser.getUsername(), finalUser.getPassword());
             }
         });
     }
---
+++ src/main/java/com/github/bobbobbob15/User.java
@@ -0,0 +1,24 @@
+package com.github.bobbobbob15;
+
+import java.io.Serializable;
+
+public class User implements Serializable {
+    public String getUsername() {
+        return username;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    private String username;
+    private String password;
+}
