aa6175849db2613467805a8f07041968c59abbcb
bobbobbob15 <48326193+bobbobbob15@users.noreply.github.com>
2021-10-07 14:27:21 -0600

    might be working once I can login

--- src/main/java/com/github/bobbobbob15/Main.java
+++ src/main/java/com/github/bobbobbob15/Main.java
@@ -3,28 +3,27 @@ package com.github.bobbobbob15;
 
 import com.google.gson.Gson;
 
-import javax.mail.Authenticator;
-import javax.mail.PasswordAuthentication;
-import javax.mail.Session;
+import javax.mail.*;
+import javax.mail.internet.*;
 import java.io.*;
 import java.util.Properties;
 import java.util.Scanner;
 
 public class Main {
     static File file = new File("src/main/java/com/github/bobbobbob15/login.txt");
-    public static void main(String[] args) throws IOException, ClassNotFoundException {
+    public static void main(String[] args) throws IOException, MessagingException {
         var gson = new Gson();
         var user = new Person();
         Scanner in = new Scanner(file);
         var fileString = in.nextLine();
         System.out.println(fileString+"is the thing");
         user = gson.fromJson(fileString, Person.class);
-        Properties prop = new Properties();
-        prop.put("mail.smtp.auth", true);
-        prop.put("mail.smtp.starttls.enable", "true");
+        Properties prop = System.getProperties();
         prop.put("mail.smtp.host", "smtp.gmail.com");
-        prop.put("mail.smtp.port", "25");
-        prop.put("mail.smtp.ssl.trust", "smtp.gmail.com");
+        prop.put("mail.smtp.port", "465");
+        prop.put("mail.smtp.ssl.enable", "true");
+        prop.put("mail.smtp.auth", "true");
+
 
         Person finalPerson = user;
         Session session = Session.getInstance(prop, new Authenticator() {
@@ -33,5 +32,21 @@ public class Main {
                 return new PasswordAuthentication(finalPerson.getUsername(), finalPerson.getPassword());
             }
         });
+        Message message = new MimeMessage(session);
+        message.setFrom(new InternetAddress(user.getUsername()));
+        message.setRecipients(
+                Message.RecipientType.TO, InternetAddress.parse("wd40bug@gmail.com"));
+        message.setSubject("Mail Subject");
+
+        String msg = "This is my first email using JavaMailer";
+
+        MimeBodyPart mimeBodyPart = new MimeBodyPart();
+        mimeBodyPart.setContent(msg, "text/html");
+
+        Multipart multipart = new MimeMultipart();
+        multipart.addBodyPart(mimeBodyPart);
+
+        message.setContent(multipart);
+        Transport.send(message);
     }
 }
