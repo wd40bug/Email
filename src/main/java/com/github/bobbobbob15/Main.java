ef8a64620e68178a148a331dbfc37ac830c45067
bobbobbob15 <wd40bug@gmail.com>
2021-10-09 18:13:42 -0600

    did some trolling

--- src/main/java/com/github/bobbobbob15/Main.java
+++ src/main/java/com/github/bobbobbob15/Main.java
@@ -1,52 +1,112 @@
 package com.github.bobbobbob15;
 
-
 import com.google.gson.Gson;
+import org.hazlewood.connor.bottema.emailaddress.EmailAddressValidator;
+import org.simplejavamail.api.email.Email;
+import org.simplejavamail.api.email.Recipient;
+import org.simplejavamail.api.mailer.config.TransportStrategy;
+import org.simplejavamail.email.EmailBuilder;
+import org.simplejavamail.mailer.MailerBuilder;
 
-import javax.mail.*;
-import javax.mail.internet.*;
-import java.io.*;
-import java.util.Properties;
+import javax.mail.Message;
+import javax.mail.internet.MimeMessage;
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.Locale;
 import java.util.Scanner;
 
 public class Main {
     static File file = new File("src/main/java/com/github/bobbobbob15/login.txt");
-    public static void main(String[] args) throws IOException, MessagingException {
+
+    public static void main(String[] args) throws IOException {
+        Scanner in = new Scanner(System.in);
+        System.out.println("Load from file?(y/n)");
+        Person user = null;
+        if(in.nextLine().charAt(0)=='y'){
+            user = getUserFromFile(file);
+        } else{
+            user = new Person(0);
+        }
+        var email = writeBlankEmail(user);
+        MailerBuilder
+                .withSMTPServer("smtp.gmail.com", 465)
+                .withSMTPServerUsername(user.getUsername())
+                .withSMTPServerPassword(user.getPassword())
+                .withTransportStrategy(TransportStrategy.SMTPS)
+                .buildMailer()
+                .sendMail(email);
+    }
+
+    public static Person getUserFromFile(File file) throws FileNotFoundException {
         var gson = new Gson();
-        var user = new Person();
-        Scanner in = new Scanner(file);
-        var fileString = in.nextLine();
-        System.out.println(fileString+"is the thing");
-        user = gson.fromJson(fileString, Person.class);
-        Properties prop = System.getProperties();
-        prop.put("mail.smtp.host", "smtp.gmail.com");
-        prop.put("mail.smtp.port", "465");
-        prop.put("mail.smtp.ssl.enable", "true");
-        prop.put("mail.smtp.auth", "true");
+        Scanner sc = new Scanner(file);
+        return gson.fromJson(sc.nextLine(), Person.class);
+    }
 
+    public static Email writeBlankEmail(Person user) {
+        Scanner in = new Scanner(System.in);
+        var recipients = getRecipients(user, in);
+        System.out.println("What is the subject of your email?");
+        var subject = in.nextLine();
+        System.out.println("What is the body of your email?");
+        var body = in.nextLine();
 
-        Person finalPerson = user;
-        Session session = Session.getInstance(prop, new Authenticator() {
-            @Override
-            protected PasswordAuthentication getPasswordAuthentication() {
-                return new PasswordAuthentication(finalPerson.getUsername(), finalPerson.getPassword());
+        return EmailBuilder.startingBlank()
+                .from(user.getUsername())
+                .to(recipients)
+                .withSubject(subject)
+                .withPlainText(body)
+                .buildEmail();
+    }
+
+    public static Recipient[] getRecipients(Person user, Scanner in) {
+        ArrayList<Recipient> recipients = new ArrayList<>();
+        boolean keepGoing = true;
+        while (keepGoing) {
+            System.out.println("Add a recipient's information(Name*,Email,Send Type*) * is optional");
+            var recipientString = in.nextLine().split(",");
+            String address = null;
+            String name = null;
+            Message.RecipientType sendType = null;
+            for (var string : recipientString) { //should work if nothing goes wrong
+                if (EmailAddressValidator.isValid(string)) {
+                    address = string;
+                } else if (string.equalsIgnoreCase("TO") || string.equalsIgnoreCase("CC") ||
+                        string.equalsIgnoreCase("BCC")) {
+                    sendType = switch (string.toLowerCase(Locale.ROOT)) {
+                        case "to" -> {
+                            yield Message.RecipientType.TO;
+                        }
+                        case "cc" -> {
+                            yield Message.RecipientType.CC;
+                        }
+                        case "bcc" -> {
+                            yield Message.RecipientType.BCC;
+                        }
+                        default -> {yield null;}
+                    };
+
+                } else {
+                    name = string;
+                }
             }
-        });
-        Message message = new MimeMessage(session);
-        message.setFrom(new InternetAddress(user.getUsername()));
-        message.setRecipients(
-                Message.RecipientType.TO, InternetAddress.parse("wd40bug@gmail.com"));
-        message.setSubject("Mail Subject");
-
-        String msg = "This is my first email using JavaMailer";
-
-        MimeBodyPart mimeBodyPart = new MimeBodyPart();
-        mimeBodyPart.setContent(msg, "text/html");
-
-        Multipart multipart = new MimeMultipart();
-        multipart.addBodyPart(mimeBodyPart);
-
-        message.setContent(multipart);
-        Transport.send(message);
+            if(address == null){
+                System.out.println("No valid email address found");
+                return getRecipients(user, in);
+            }
+            if(sendType == null){
+                sendType = Message.RecipientType.TO;
+            }
+            recipients.add(new Recipient(name,address,sendType));
+            System.out.println("Add more recipients?(y/n)");
+            char userInput = in.nextLine().toLowerCase(Locale.ROOT).charAt(0);
+            if (userInput == 'n') {
+                keepGoing = false;
+            }
+        }
+       return recipients.toArray(Recipient[]::new);
     }
 }
+
--- src/main/java/com/github/bobbobbob15/Person.java
+++ src/main/java/com/github/bobbobbob15/Person.java
@@ -1,8 +1,33 @@
 package com.github.bobbobbob15;
 
-import java.io.Serializable;
+import com.google.gson.Gson;
+import org.hazlewood.connor.bottema.emailaddress.EmailAddressValidator;
+
+import javax.swing.*;
+import java.io.*;
+import java.util.Scanner;
 
 public class Person implements Serializable {
+    Person(int i) throws IOException {
+        constructor();
+    }
+    Person(){
+
+    }
+
+    private void constructor() throws IOException {
+        Scanner in = new Scanner(System.in);
+        System.out.println("Hello! enter your email");
+        username = in.nextLine();
+        if (!EmailAddressValidator.isValid(username)) {
+            System.out.println("invalid input");
+            constructor();
+        }
+        var console = System.console();
+        password = readPwd();
+
+    }
+
     public String getUsername() {
         return username;
     }
@@ -17,6 +42,27 @@ public class Person implements Serializable {
 
     public void setPassword(String password) {
         this.password = password;
+    }
+    private static String readPwd() throws IOException {
+        Console c=System.console();
+        if (c==null) { //IN IDE
+            System.out.print("Password: ");
+            InputStream in=System.in;
+            int max=50;
+            byte[] b=new byte[max];
+
+            int l= in.read(b);
+            l--;//last character is \n
+            if (l>0) {
+                byte[] e=new byte[l];
+                System.arraycopy(b,0, e, 0, l);
+                return new String(e);
+            } else {
+                return null;
+            }
+        } else { //Outside Eclipse IDE
+            return new String(c.readPassword("Password: "));
+        }
     }
 
     private String username;
