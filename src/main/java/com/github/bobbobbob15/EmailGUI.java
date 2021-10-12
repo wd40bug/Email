cc54ab912f1502fc2f6824c03a2fa090af0dca58
bobbobbob15 <wd40bug@gmail.com>
2021-10-11 21:05:01 -0600

    fixed warnings

--- src/main/java/com/github/bobbobbob15/EmailGUI.java
+++ src/main/java/com/github/bobbobbob15/EmailGUI.java
@@ -1,16 +1,11 @@
 package com.github.bobbobbob15;
 
-import com.google.gson.Gson;
-
 import javax.swing.*;
 import java.awt.*;
-import java.awt.event.*;
-import java.io.File;
+import java.awt.event.ItemEvent;
 import java.io.FileNotFoundException;
-import java.io.IOException;
-import java.util.Scanner;
-import javax.jnlp.*;
 
+@SuppressWarnings("unused")
 public class EmailGUI {
     private JPanel rootPanel;
     private JPanel loginPanel;
@@ -37,46 +32,32 @@ public class EmailGUI {
     Person person;
 
     public EmailGUI() {
-        checkBox1.addItemListener(new ItemListener() {
-            @Override
-            public void itemStateChanged(ItemEvent e) {
-                if(e.getStateChange() == ItemEvent.SELECTED){
-                    passwordField1.setEchoChar((char)0);
-                }else if(e.getStateChange() == ItemEvent.DESELECTED){
-                    passwordField1.setEchoChar('\u2022');
-                }
+        checkBox1.addItemListener(e -> {
+            if(e.getStateChange() == ItemEvent.SELECTED){
+                passwordField1.setEchoChar((char)0);
+            }else if(e.getStateChange() == ItemEvent.DESELECTED){
+                passwordField1.setEchoChar('\u2022');
             }
         });
-        loginButton.addActionListener(new ActionListener() {
-            @Override
-            public void actionPerformed(ActionEvent e) {
-                cl.show(rootPanel,"Card2");
-                rootPanel.revalidate();
-            }
+        loginButton.addActionListener(e -> {
+            cl.show(rootPanel,"Card2");
+            rootPanel.revalidate();
         });
-        composeButton.addActionListener(new ActionListener() {
-            @Override
-            public void actionPerformed(ActionEvent e) {
-                cl.show(rootPanel,"Card3");
-            }
-        });
-        loginFromFileButton.addActionListener(new ActionListener() {
-            @Override
-            public void actionPerformed(ActionEvent e) {
-                JFileChooser jFileChooser = new JFileChooser("Documents");
-                jFileChooser.showOpenDialog(null);
-                int r = jFileChooser.showOpenDialog(null);
+        composeButton.addActionListener(e -> cl.show(rootPanel,"Card3"));
+        loginFromFileButton.addActionListener(e -> {
+            JFileChooser jFileChooser = new JFileChooser("Documents");
+            jFileChooser.showOpenDialog(null);
+            int r = jFileChooser.showOpenDialog(null);
 
-                // if the user selects a file
-                if (r == JFileChooser.APPROVE_OPTION) {
-                    var file = jFileChooser.getSelectedFile();
-                    try {
-                        person = Main.getUserFromFile(file);
-                    } catch (FileNotFoundException ex) {
-                        ex.printStackTrace();
-                    }
-                    cl.show(rootPanel,"Card2");
+            // if the user selects a file
+            if (r == JFileChooser.APPROVE_OPTION) {
+                var file = jFileChooser.getSelectedFile();
+                try {
+                    person = Main.getUserFromFile(file);
+                } catch (FileNotFoundException ex) {
+                    ex.printStackTrace();
                 }
+                cl.show(rootPanel,"Card2");
             }
         });
     }
